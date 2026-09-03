461bb16736fa2d1edb66355c649448eb1c75d9bf
Rakha Naya Dwirangga <rkhnayadwirangga@gmail.com>
2026-09-03 09:49:56 +0700

    Add DOMMatrix and browser polyfills for PDF parsing

--- pages/api/parse-pdf.js
+++ pages/api/parse-pdf.js
@@ -1,3 +1,28 @@
+// Polyfill browser APIs required by pdfjs-dist in Node.js serverless runtimes
+if (typeof globalThis.DOMMatrix === 'undefined') {
+  globalThis.DOMMatrix = class DOMMatrix {
+    constructor() {
+      this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
+    }
+  };
+}
+
+if (typeof globalThis.ImageData === 'undefined') {
+  globalThis.ImageData = class ImageData {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.data = new Uint8ClampedArray(width * height * 4);
+    }
+  };
+}
+
+if (typeof globalThis.Path2D === 'undefined') {
+  globalThis.Path2D = class Path2D {
+    addPath() {}
+  };
+}
+
 export const config = {
   api: {
     bodyParser: {
