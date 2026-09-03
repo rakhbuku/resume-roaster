93cfec0ea73a8a94a00fbc8dea2ca7618750ebcf
Rakha Naya Dwirangga <rkhnayadwirangga@gmail.com>
2026-09-03 08:50:39 +0700

    Fix missing globals.css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { SessionProvider } from 'next-auth/react';
-import '../styles/globals.css'; // Keep your existing global styles import if present
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
